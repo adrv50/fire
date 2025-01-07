71351aff5e15bac7d0b9a4bf52e586cd52601769
adrv50 <176711389+adrv50@users.noreply.github.com>
2025-01-07 18:09:49 +0900

    fix scope

--- src/Sema/SemaScope.cpp
+++ src/Sema/SemaScope.cpp
@@ -131,11 +131,15 @@ size_t Sema::Scope::find_func(Sema* S, Vec<TypeInfo>* template_args,
 
         auto ctx_keep = S->ctx;
 
-        S->ctx.cur_scope = func;
+        S->ctx.cur_scope = func->parent;
+
         S->ctx.cur_func = func;
+
         S->ctx.is_in_func = true;
 
+        fn->nd_func_is_template = false;
         S->check_func(fn);
+        fn->nd_func_is_template = true;
 
         alertmsg(S->eval_type_ti(fn->nd_func_result_type).to_string());
 
